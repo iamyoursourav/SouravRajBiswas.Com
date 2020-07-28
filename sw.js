ba1f0be106b49a85bf1b7a318df1ac9482d8e119
Sourav Biswas <iamyoursourav@gmail.com>
2020-07-28 16:16:50 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var appVersion = 'v2.00';
+var appVersion = 'v1.00';
 var urlsToCache = [
     'https://1.bp.blogspot.com/-PyzQRd1mKC4/XwTLEivYvFI/AAAAAAAADgk/JUmGVGZV3igFHkC20o5aLR9fYocepBCngCK4BGAYYCw/w300/Sourav-Raj-Biswas-Logo.png',
     'https://fonts.gstatic.com/s/nunitosans/v5/pe0qMImSLYBIv1o4X1M8cfe5.woff',
@@ -11,7 +11,7 @@ var urlsToCache = [
     'https://checkout.razorpay.com/v1/checkout.js',
 ];
 
-var worker = worker;
+var worker = 'worker';
 var workerUrl = 'https://cdn.souravrajbiswas.com/sw.js';
 
 self.addEventListener('install', function(event) {
